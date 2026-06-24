8310eeca0f45e1098fdaf5e9f9e881dd10daf8c8
SIDDHARTH SINGH JADON <siddharth.s.jadon1706@gmail.com>
2026-06-24 15:00:43 +0530

    Delete sql/01_database_setup.sql

--- sql/01_database_setup.sql
+++
@@ -1,8 +0,0 @@
------------------- Create Database --------------------
-
-CREATE DATABASE ecommerce_analysis;
-GO
-
-USE ecommerce_analysis;
-GO
--------------------------------------------------------
